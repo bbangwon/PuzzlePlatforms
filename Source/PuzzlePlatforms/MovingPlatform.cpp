3c4e533161676c011ad86508a83e185d75151974
youngil <youngil@placeb.com>
2024-03-05 09:58:13 +0900

    주석 추가 정리

--- Source/PuzzlePlatforms/MovingPlatform.cpp
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -19,12 +19,14 @@ void AMovingPlatform::BeginPlay()
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Display, TEXT("MovingPlatform BeginPlay"));
 
+	//서버에서만 실행 : 클라이언트로 복제 설정
 	if (HasAuthority())
 	{
 		SetReplicates(true);
 		SetReplicateMovement(true);
 	}
 
+	//시작 위치와 움직일 위치를 설정
 	GlobalStartLocation = GetActorLocation();
 	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);	
 }
@@ -39,6 +41,7 @@ void AMovingPlatform::Tick(float DeltaSeconds)
 		FVector Location = GetActorLocation();
 		FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
 
+		//움직인 벡터의 길이를 비교하여 방향을 바꿔줌
 		float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
 		float JourneyTravelled = (Location - GlobalStartLocation).Size();
 		if (JourneyTravelled >= JourneyLength)
