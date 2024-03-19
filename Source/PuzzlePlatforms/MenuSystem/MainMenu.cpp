a0219f223afb91441edd705fddc1fb16dec6d00a
youngil <youngil@placeb.com>
2024-03-19 20:06:21 +0900

    이니셜라이저 및 UButton 콜백

--- Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
+++ Source/PuzzlePlatforms/MenuSystem/MainMenu.cpp
@@ -2,4 +2,22 @@
 
 
 #include "MainMenu.h"
+#include "Components/Button.h"
+
+bool UMainMenu::Initialize()
+{
+	bool bSuccess = Super::Initialize();
+	if(!bSuccess) return false;
+
+	if(!ensure(Host != nullptr)) return false;
+	Host->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+
+	return true;
+}
+
+void UMainMenu::HostServer()
+{
+	UE_LOG(LogTemp, Warning, TEXT("호스트 서버를 설정해야지!"));
+}
+
 
